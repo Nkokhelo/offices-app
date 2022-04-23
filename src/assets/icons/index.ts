3b904812feffdc51b45c50bc9fcab1aa10d93145
Nhlakanipho Nkokhelo <N.N.TEMBE@outlook.com>
2022-04-23 14:07:37 +0200

    update assets

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,40 @@
+
+import { ReactComponent as ArrowIcon } from './arrow.svg';
+import { ReactComponent as CloseIcon } from './close.svg';
+import { ReactComponent as EmailIcon } from './email.svg';
+import { ReactComponent as BackIcon } from './back.svg';
+import { ReactComponent as LeftIcon } from './left.svg';
+import { ReactComponent as LocationIcon } from './location.svg';
+import { ReactComponent as PenIcon } from './pen.svg';
+import { ReactComponent as PeopleIcon } from './people.svg';
+import { ReactComponent as PhoneIcon } from './phone.svg';
+import { ReactComponent as PlusIcon } from './plus.svg';
+import { ReactComponent as Profile0Icon } from './profile0.svg';
+import { ReactComponent as Profile1Icon } from './profile1.svg';
+import { ReactComponent as Profile2Icon } from './profile2.svg';
+import { ReactComponent as Profile3Icon } from './profile3.svg';
+import { ReactComponent as Profile4Icon } from './profile4.svg';
+import { ReactComponent as Profile5Icon } from './profile5.svg';
+import { ReactComponent as Profile6Icon } from './profile6.svg';
+import { ReactComponent as PlainPIcon } from './peoplePlain.svg';
+
+export {
+  ArrowIcon,
+  BackIcon,
+  CloseIcon,
+  EmailIcon,
+  LeftIcon,
+  LocationIcon,
+  PenIcon,
+  PlainPIcon,
+  PeopleIcon,
+  PhoneIcon,
+  PlusIcon,
+  Profile0Icon,
+  Profile1Icon,
+  Profile2Icon,
+  Profile3Icon,
+  Profile4Icon,
+  Profile5Icon,
+  Profile6Icon,
+  }
